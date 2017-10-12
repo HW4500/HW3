40f0fe59bff472f8bfa97dde2f79f50df1acb38e
Marin-Bergerot <32662553+Marin-Bergerot@users.noreply.github.com>
2017-10-12 15:22:20 -0400

    New analyze.py, quite different to work with qp

--- analyze.py
+++ analyze.py
@@ -1,15 +1,15 @@
 import sys
 import csv
+import os
 #To read : here we consider that day 0 is the initial day, therefore if N=1, we give the return for the first day, (p(1)-p(0))/p(0)
 #because of that, N cannot go over 249
-#The output.write's written as commentaries can replace the present writing loop to have the assets returns and then the covariance matrix
 
-
-if len(sys.argv) != 5:
-	sys.exit("usage: analyze.py datafilename outputfilename number_of_asset number_of_days\n")
+if len(sys.argv) != 7:
+	sys.exit("usage: analyze.py datafilename outputfilename number_of_asset number_of_days namein nameout\n")
 
 #now reading and memorizing the values of the number of assets or the number of days
 l=[]
+print(sys.argv[3])
 try:
 	numasset=int(sys.argv[3])
 	numday=int(sys.argv[4])
@@ -35,12 +35,16 @@ try:
 except IOError:
 	print("Cannot open outputfile file %s for writing\n" % sys.argv[2])
 	
-
-print("now writing output to file", sys.argv[2])
-output.write("number_of_assets: "+str(numasset)+" number_of_days: "+str(numday)+"\n\n")	
-	
-#creating and calculating average asset returns list
+#variables
+lambdain=10
 avg_asset_return=[]
+upper=max(0.01,2.0/numasset)
+lower=0
+	
+print("now writing output to file", sys.argv[2])
+output.write("n "+str(numasset)+"\n\nj_lower_upper_mu\n\n")	
+	
+#calculating average asset returns list
 for i in range(numasset):
 	asset_prices_i=l[i][:numday+1]
 	returns=0
@@ -48,12 +52,12 @@ for i in range(numasset):
 		returns+=(float(asset_prices_i[t+1])-float(asset_prices_i[t]))/float(asset_prices_i[t])
 	returns=returns/numday
 	avg_asset_return.append(returns)
-	#output.write("Average_return_of_asset_"+str(i+1)+": "+str(avg_asset_return[i])+"\n")
+	output.write(str(i)+" "+str(lower)+"   "+str(upper)+"  "+str(avg_asset_return[i])+"\n")
 
-
+output.write("\nlambda "+str(lambdain)+"\n\n")
 	
 #creating and calculating covariance matrix	
-#output.write("\nCovariance matrix: \n\n")
+output.write("Covariance_matrix: \n\n")
 if numday==1:
 	#Not sure of the covariance with just one term, but I choose 0 by default for now
 	covariance_matrix=[[0]*numasset]*numasset
@@ -70,16 +74,29 @@ else:
 			#as it is a sample, we divide by T-1 and not by T
 			covariance=sum/(numday-1)
 			covariance_matrix[i].append(covariance)
-			#output.write(str(covariance_matrix[i][j])+" ")
-		#output.write("\n")
+			output.write(str(covariance_matrix[i][j])+" ")
+		output.write("\n")
 
-#writing loop: writing the results in a .txt file
-for i in range(numasset):
-	output.write("Average_return_of_asset_"+str(i+1)+": "+str(avg_asset_return[i])+"   Correlation_with_respect_to_assets_1_to_K: ")
-	for j in range(numasset):
-		output.write(str(covariance_matrix[i][j])+" ")
-	output.write("\n")
 
+
+output.write("\nEND")
+	
 output.close
+print("renaming ", sys.argv[5], "into ", sys.argv[6])
+os.rename(sys.argv[5], sys.argv[6])
+
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+abscissa=[]
+for i in range(numasset):
+	abscissa.append(covariance_matrix[i][i])
+ordinate=avg_asset_return
+
+plt.plot(abscissa,ordinate,'rx')
+plt.xlabel('volatility')
+plt.ylabel('average rate of return')
+plt.savefig('plot.png')
 
 
