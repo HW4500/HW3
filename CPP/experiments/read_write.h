083bb02a5a8d1bb0ddbed445ee44857b514376d1
Marin-Bergerot <32662553+Marin-Bergerot@users.noreply.github.com>
2017-10-13 02:27:12 -0400

    Create read_write.h

---
+++ CPP/experiments/read_write.h
@@ -0,0 +1,3 @@
+
+int readit(char *nameoffile, int *addressofn, double **, double **, double **, double *, double **);
+int presentation(double objective, double *x,double *mu, double *covariance,int n,double avgret);
