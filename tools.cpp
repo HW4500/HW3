135954517fa91089c4786fea2172cda6b42ba922
Marin-Bergerot <32662553+Marin-Bergerot@users.noreply.github.com>
2017-10-13 01:59:49 -0400

    Add files via upload

---
+++ tools.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm> 
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <windows.h>
+
+
+double get_min(double a, double b)
+{
+	if(a<b)
+		return a;
+	return b;
+}
+
+
+char does_it_exist(char *filename)
+{
+	struct stat buf;
+
+	// the function stat returns 0 if the file exists
+
+	if (0 == stat(filename, &buf)){
+		return 1;
+	}
+	else return 0;
+}
