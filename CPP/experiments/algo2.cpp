607f6fbd1bff41ed21296d21fdaf7bae5750177f
Marin-Bergerot <32662553+Marin-Bergerot@users.noreply.github.com>
2017-10-13 02:28:28 -0400

    Update algo2.cpp

--- CPP/experiments/algo2.cpp
+++ CPP/experiments/algo2.cpp
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm> 
 
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <windows.h>
+
+#include "algo.h"
+#include "tools.h"
+
+int algo2()
+{
+  
+}
