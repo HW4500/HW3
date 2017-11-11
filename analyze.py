fef1961cdfacc3b6cce6965531bc541438a71de6
Marin-Bergerot <32662553+Marin-Bergerot@users.noreply.github.com>
2017-11-10 23:50:03 -0500

    Add files via upload

---
+++ analyze.py
@@ -0,0 +1,87 @@
+import sys
+import csv
+import os
+import time
+from math import *
+#To read : here we consider that day 0 is the initial day, therefore if N=1, we give the return for the first day, (p(1)-p(0))/p(0)
+#because of that, N cannot go over 249
+
+departure=time.time()
+
+if len(sys.argv) != 7:
+	sys.exit("usage: analyze.py datafilename outputfilename number_of_asset number_of_days namein nameout\n")
+
+#now reading and memorizing the values of the number of assets or the number of days
+l=[]
+try:
+	numasset=int(sys.argv[3])
+	numday=int(sys.argv[4])
+except ValueError:
+	sys.exit("Cannot read the number of assets or the number of days, must be integers")
+
+if numasset>1000 or numday>=250:
+	sys.exit("number of assets or number of days out of range : assets from 1 to 1000, days from 0 to 249")
+
+#now reading and copying the data from the input file
+try: 
+	datafile= open(sys.argv[1], 'r') 
+	reader = csv.reader(datafile)
+	for row in reader:
+		l.append(row)
+	datafile.close
+except IOError:
+	sys.exit("Cannot open file %s\n" % sys.argv[1])
+
+#now opening the output file to write
+try:
+	output = open(sys.argv[2], 'w') # opens the file
+except IOError:
+	print("Cannot open outputfile file %s for writing\n" % sys.argv[2])
+	
+#variables
+avg_asset_return=[]
+
+	
+print("now writing output to file", sys.argv[2])
+	
+#calculating average asset returns list
+for i in range(numasset):
+	asset_prices_i=[0]*(numday+1)
+	for j in range(numday+1):
+		l[i][j]=float(l[i][j])
+		asset_prices_i[j]=l[i][j]
+	returns=0
+	for t in range(numday):
+		returns+=((asset_prices_i[t+1])-(asset_prices_i[t]))/(asset_prices_i[t])
+	returns=returns/numday
+	avg_asset_return.append(returns)
+	
+#creating and calculating covariance matrix	
+output.write("Covariance_matrix: \n\n")
+covariance_matrix=[0]*(numasset*numasset)
+#Not sure of the covariance with just one term, but I choose 0 by default for now
+if numday!=1:
+	for i in range(numasset):
+		asset_prices_i=l[i][:numday+1]
+		for j in range(numasset):
+			asset_prices_j=l[j][:numday+1]
+			sum=0
+			for t in range(numday):
+				sum+=((((asset_prices_i[t+1])-(asset_prices_i[t]))/(asset_prices_i[t]))-avg_asset_return[i])*((((asset_prices_j[t+1])-(asset_prices_j[t]))/(asset_prices_j[t]))-avg_asset_return[j])
+			#as it is a sample, we divide by T-1 and not by T
+			covariance=sum/(numday-1)
+			covariance_matrix[i*numasset+j]=covariance
+			output.write(str(covariance)+" ")
+		output.write("\n")
+
+		
+
+output.write("\nEND")
+	
+output.close
+print("renaming ", sys.argv[5], "into ", sys.argv[6])
+os.rename(sys.argv[5], sys.argv[6])
+
+
+end=time.time()
+print("chrono= "+str(end-departure)+'s, or '+str(floor((end-departure)/60))+"m and "+str((end-departure)%60)+"s")
